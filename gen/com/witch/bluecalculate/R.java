945dee048d7e1660b906aae7e854847f94f1ae1c
Robert Irribarren <robertirribarren@gmail.com>
2013-08-01 20:07:13 -0700

    WIP

--- gen/com/witch/bluecalculate/R.java
+++ gen/com/witch/bluecalculate/R.java
@@ -20,6 +20,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+<<<<<<< HEAD
         public static final int TextView01=0x7f090003;
         public static final int addition=0x7f090005;
         public static final int buttonConnect=0x7f090000;
@@ -28,6 +29,15 @@ public final class R {
         public static final int menu_settings=0x7f090007;
         public static final int subtraction=0x7f090006;
         public static final int textView_Answer=0x7f090001;
+=======
+        public static final int TextView01=0x7f090001;
+        public static final int addition=0x7f090003;
+        public static final int editText1=0x7f090004;
+        public static final int editText2=0x7f090002;
+        public static final int menu_settings=0x7f090006;
+        public static final int subtraction=0x7f090005;
+        public static final int textView_Answer=0x7f090000;
+>>>>>>> origin/jessica
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +52,10 @@ public final class R {
         public static final int hello_world=0x7f060002;
         public static final int menu_settings=0x7f060001;
         public static final int minus=0x7f060005;
+<<<<<<< HEAD
+=======
+        public static final int send_problemButton=0x7f060006;
+>>>>>>> origin/jessica
         public static final int userDirection_InputText=0x7f060003;
     }
     public static final class style {
--- src/com/witch/bluecalculate/MainActivity.java
+++ src/com/witch/bluecalculate/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +32,19 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 	private BluetoothHelper bluetoothHelper;
 	private TextView tv_Answer;
 	public static Activity activity;
+<<<<<<< HEAD
+=======
+	/*start*/
+	private Button addButton, subButton;
+	private EditText text1, text2;
+	private TextView resultBox;
+	private MotionEvent event = null;
+	private int number1, number2;
+	public String finalDisplay;
+	private String bundleStringOut;
+	/*end*/
+	
+>>>>>>> origin/jessica
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,10 +53,47 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 		
 		tv_Answer = (TextView)findViewById(R.id.textView_Answer);
 		tv_Answer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
+<<<<<<< HEAD
 		buttonConnect = (Button)findViewById(R.id.buttonConnect);
 		
 		bluetoothHelper = new BluetoothHelper(MainActivity.this);
 		buttonConnect.setOnTouchListener(this);
+=======
+		
+		bluetoothHelper = new BluetoothHelper(MainActivity.this);
+	/*start*/	
+		addButton = (Button) findViewById(R.id.addition);
+		subButton= (Button) findViewById(R.id.subtraction);
+		text1= (EditText) findViewById(R.id.editText1);
+		text2= (EditText) findViewById(R.id.editText2);
+		resultBox= (TextView) findViewById(R.id.TextView01);
+		
+		addButton.setOnTouchListener(new OnTouchListener(){
+		
+		@Override
+		public boolean onTouch(View arg0, MotionEvent event) {	
+		if (event.getAction()!= MotionEvent.ACTION_DOWN)
+			return false;
+		
+		setNumbers_finalDisplay_bundleStringOut('+');
+		sendIT(bundleStringOut);
+		return false;
+		}	
+		});
+		
+		subButton.setOnTouchListener(new OnTouchListener(){
+		
+		@Override
+		public boolean onTouch(View arg0, MotionEvent event){
+		if (event.getAction()!= MotionEvent.ACTION_DOWN)
+			return false;
+		setNumbers_finalDisplay_bundleStringOut('-');
+		sendIT(bundleStringOut);
+		return false;
+		}
+		});
+		/*end*/
+>>>>>>> origin/jessica
 		
 		//start server when its loaded 
 		bluetoothHelper.initServer();
@@ -56,6 +107,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 	    if (requestCode == INTENTCODE.SET_MATH_PROBLEM.ordinal()) {
 	        // Make sure the request was successful
 	        if (resultCode == RESULT_OK) {
+<<<<<<< HEAD
 	        	String sendMe = data.getStringExtra("sendString");
 	        	bluetoothHelper.setSendMessage(sendMe);
 	        	if (bluetoothHelper.isConnectedAsClient()==false)
@@ -67,6 +119,21 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 	}
 	
 	
+=======
+	        	
+	        }
+	    }
+	}
+	
+	public void sendIT(String sendMe) {
+    	bluetoothHelper.setSendMessage(sendMe);
+    	if (bluetoothHelper.isConnectedAsClient()==false)
+    		bluetoothHelper.initClient();
+    	else
+    		bluetoothHelper.sendMessage(sendMe);
+	}
+	
+>>>>>>> origin/jessica
 	public void updateMainTextAnswer(String s)
 	{
 		/*String old = textViewOutput.getText().toString();
@@ -89,6 +156,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 		if (event.getAction() != MotionEvent.ACTION_UP) return false;
 		switch(v.getId())
 		{
+<<<<<<< HEAD
 		case R.id.buttonConnect:
 			Log.i(tag,"Client button clicked starting math activity");
 			//the client will be started after the intent is returned
@@ -99,10 +167,44 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 			//Log.i(tag,"Server button clicked");
 			//bluetoothHelper.initServer();
 			//break;
+=======
+>>>>>>> origin/jessica
 		default:
 			break;
 		}
 		return false;
 	}
+<<<<<<< HEAD
 
+=======
+	/*start*/
+	private void setNumbers_finalDisplay_bundleStringOut(char operator){
+		number1=0;
+		number2=0;
+		try {
+		number1 = Integer.parseInt(text1.getText().toString());
+		number2 = Integer.parseInt(text2.getText().toString());
+		} catch (NumberFormatException nfe){}
+		String op= "" + operator; //making an equivalent String
+		finalDisplay= returnAnswer(number1, number2, operator).toString();
+		bundleStringOut = BluetoothMessenger.makeMessage(number1, number2, op);
+	}
+	
+	public Integer returnAnswer(int a, int b, char operator){
+		int ans_int=0;
+		switch (operator){
+			case '+':
+				ans_int= a+b;
+				break;
+			case '-':
+				ans_int=a-b;
+				break;
+			default:
+				break;
+		}
+		Integer answer= Integer.valueOf(ans_int);
+		return answer;
+	}
+	/*end*/
+>>>>>>> origin/jessica
 }
